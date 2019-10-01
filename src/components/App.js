6ec0ac3b5e3544df0e16718ea45b6fb14723813a
effiong-jr <f.young.de@gmail.com>
2019-10-01 14:51:07 +0100

    Added footer

--- src/components/App.js
+++ src/components/App.js
@@ -7,18 +7,22 @@ import Cart from './Cart';
 import Login from '../components/user/Login';
 import SignUp from '../components/user/Signup';
 import './App.css';
+import Footer from './Footer';
 
 const App = (props) => (
     <div className="App">
         <Router>
             <Header />
-            <main className="container">
-                <Route exact path="/" render={()=> <LandingPage />} />
-                <Route path="/watch/:name" component={FullWatchDetails} />
-                <Route path="/your-cart/" component={Cart} />
-                <Route path="/user/login/" component={Login} />
-                <Route path="/user/signup/" component={SignUp}/>
-            </main>
+            <div id="mainContainer">
+                <main className="container">
+                    <Route exact path="/" render={()=> <LandingPage />} />
+                    <Route path="/watch/:name" component={FullWatchDetails} />
+                    <Route path="/your-cart/" component={Cart} />
+                    <Route path="/user/login/" component={Login} />
+                    <Route path="/user/signup/" component={SignUp}/>
+                </main>
+            </div>
+            <Footer />
         </Router>
     </div>
 );
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './footer.css';
+
+export default function Footer() {
+    return (
+        <div className='footer'>
+            <h5>Casio Shopping App</h5>
+            <small>Created for fun by f.young&copy; </small>
+        </div>
+    )
+}
